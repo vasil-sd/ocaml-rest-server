26de9e798c12ddda5b36d2c6971925055b6361b2
Vasil S Dyadov <vasil.s.d@gmail.com>
2018-08-30 22:11:13 +0300

    Initital support of swagger

--- lib/api.mli
+++ lib/api.mli
@@ -4,44 +4,61 @@ type response =
       -> ?text:string
       -> unit
       -> unit
-  ; response_ok: unit -> unit
+  ; response_ok: ?text:string -> unit -> unit
   ; response_json: Yojson.Safe.json -> unit
   ; get_path_var: Core.String.t -> Core.String.t option }
 
 exception Path_Conflict
 
+type 'a rest_handler = 'a Httpaf.Reqd.t -> Rest_path.Var.vars -> unit
+
+type _ handler =
+  | Without_request : (response -> unit) -> [`Without_request] handler
+  | With_request : (response -> Yojson.Safe.json -> unit) -> [`With_request] handler
+
+type any_handler = Handler: 'a handler -> any_handler
+
 type 'a handlers
 
 val empty : 'a handlers
 
+val append : 'a handlers -> 'a handlers -> 'a handlers
+
 val register :
      Httpaf.Method.t
   -> Core.String.t
-  -> ('a Httpaf.Reqd.t -> Rest_path.Var.vars -> unit)
+  -> 'a rest_handler
+  -> 'a handlers
+  -> 'a handlers
+
+val register_any :
+     Httpaf.Method.t
+  -> Core.String.t
+  -> any_handler
   -> 'a handlers
   -> 'a handlers
 
 val get :
      Core.String.t
-  -> (response -> unit)
+  -> [`Without_request] handler
   -> 'a handlers
   -> 'a handlers
 
 val put :
      Core.String.t
-  -> (response -> Yojson.Safe.json -> unit)
+  -> [`With_request] handler
   -> 'a handlers
   -> 'a handlers
 
 val post :
      Core.String.t
-  -> (response -> Yojson.Safe.json -> unit)
+  -> [`With_request] handler
   -> 'a handlers
   -> 'a handlers
 
 val delete :
      Core.String.t
-  -> (response -> unit)
+  -> [`Without_request] handler
   -> 'a handlers
   -> 'a handlers
 
--- lib/http_json.mli
+++ lib/http_json.mli
@@ -1,6 +1,6 @@
 val response_error :
   ?error:Httpaf.Status.standard -> ?text:string -> 'a Httpaf.Reqd.t -> unit
-val response_ok : 'a Httpaf.Reqd.t -> unit
+val response_ok : ?text:string -> 'a Httpaf.Reqd.t -> unit
 val response_json : 'a Httpaf.Reqd.t -> Yojson.Safe.json -> unit
 val request_json :
   'a Httpaf.Reqd.t ->
